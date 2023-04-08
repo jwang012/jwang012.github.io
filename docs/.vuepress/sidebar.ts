3c2fdbd028d633dd5b1465cd40f0aa0b9e9c0e1e
James <969004192@qq.com>
2023-04-08 23:49:37 +0800

    删除健身计划

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,7 +3,6 @@ import { sidebar } from "vuepress-theme-hope";
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar([
   "/DailyRoutine",
-  "/Fitness",
   // 读书笔记架构更换到 docsify，不能使用相对链接
   { text: "读书笔记", icon: "read", link: "https://newzone.top/reading/" },
   // 指定显示页面
